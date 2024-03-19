df9a8eaca12fab6ec25b7d827f79cbb98dc94d39
Suhana Sajid <73544349+Suhana66@users.noreply.github.com>
2024-03-19 21:47:37 +0400

    added command argument list memory management, eval function prototype

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 10:06:28 by susajid           #+#    #+#             */
-/*   Updated: 2024/03/19 18:03:41 by susajid          ###   ########.fr       */
+/*   Updated: 2024/03/20 09:21:32 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(void)
 	char	*prompt;
 	char	*input;
 	char	**cmd_argv;
+	size_t	i;
 
 	prompt = getcwd(NULL, 0);
 	if (!prompt)
@@ -31,10 +32,18 @@ int	main(void)
 			break ;
 		add_history(input);
 		cmd_argv = parse_cli_input(input);
-		(void)cmd_argv;
-		// TODO: cmd_argv memory management
+		eval(cmd_argv);
+		i = 0;
+		while (cmd_argv[i])
+			free(cmd_argv[i++]);
+		free(cmd_argv);
 		free(input);
 	}
 	free(prompt);
 	return (0);
+}
+
+void	eval(char **cmd_argv)
+{
+	(void)cmd_argv;
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 11:30:11 by susajid           #+#    #+#             */
-/*   Updated: 2024/03/19 14:26:52 by susajid          ###   ########.fr       */
+/*   Updated: 2024/03/19 21:45:33 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # include <readline/readline.h>
 
 char	**parse_cli_input(char *input);
+void	eval(char **cmd_argv);
 
 #endif /* MINISHELL_H */
