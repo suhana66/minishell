93a5f1cf4c4043c91b01f8fb64e1d5518725ee6a
Saunthos K Muneer <132748528+saunthosk@users.noreply.github.com>
2024-04-29 17:33:05 +0000

    added structs into header for env and pwd

--- minishell.h
+++ minishell.h
@@ -15,7 +15,24 @@
 
 # include "libft.h"
 # include <stdio.h>
-# include <readline/readline.h>
+# include <readline/readlin
+
+typedef struct s_env
+{
+	char			**path;
+	char			*value;
+	char			*key;
+	struct s_env	*next;
+}				t_env;
+
+typedef struct s_info
+{
+	int				no_path;
+	char			*pwd;
+	char			*old_pwd;
+	t_env			*envv;
+}				t_info;
+
 
 char	**split_cli_input(char *input, char *delimiters, char *enclosers);
 int		expand_cmd_arg(char **cmd_argv);
