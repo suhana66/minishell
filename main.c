f23d8958edd6ed5dd3f2ca630de563d1e868072c
Suhana Sajid <73544349+Suhana66@users.noreply.github.com>
2024-04-30 16:05:52 +0400

    added error message for executable arguments

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:44:04 by susajid           #+#    #+#             */
-/*   Updated: 2024/04/30 15:52:32 by susajid          ###   ########.fr       */
+/*   Updated: 2024/04/30 16:05:02 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void)argc;
+	if (argc != 1)
+		return (ft_putendl_fd("usage: ./minishell", 2), 1);
 	(void)argv;
 	(void)envp;
 	return (0);
