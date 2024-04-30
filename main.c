ed710476adb0ea137f818c9f9910b8c8b4a8968b
Suhana Sajid <73544349+Suhana66@users.noreply.github.com>
2024-04-30 17:19:33 +0400

    determined delimiters and enclosers

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:44:04 by susajid           #+#    #+#             */
-/*   Updated: 2024/04/30 17:09:58 by susajid          ###   ########.fr       */
+/*   Updated: 2024/04/30 17:18:42 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,16 @@ int	main(int argc, char **argv, char **envp)
 	return (0);
 }
 
+// Bash delimiters can be found in the variable IFS (Internal Field Separator)
 t_lexer	*lexer(char *input)
 {
+	char	*delimiters;
+	char	*enclosers;
+
+	delimiters = " \t\n";
+	enclosers = "\"\'";
+	(void)delimiters;
+	(void)enclosers;
 	(void)input;
 	return (NULL);
 }
