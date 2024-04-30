cf6344919f8d9c13bd1cb3e8da86eb09a53cfe3c
Suhana Sajid <73544349+Suhana66@users.noreply.github.com>
2024-04-30 17:11:28 +0400

    added lexer prototype

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 11:30:11 by susajid           #+#    #+#             */
-/*   Updated: 2024/04/30 16:36:19 by susajid          ###   ########.fr       */
+/*   Updated: 2024/04/30 17:09:33 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,24 @@
 # include <readline/readline.h>
 
 # define READLINE_MSG	"\033[1;32mminishell$ \033[0m"
+
+enum e_token
+{
+	WORD,
+	PIPE,
+	GREAT,
+	GREATGREAT,
+	LESS,
+	LESSLESS
+};
+
+typedef struct s_lexer
+{
+	enum e_token	token_type;
+	char			*str;
+	struct s_lexer	*next;
+}	t_lexer;
+
+t_lexer	*lexer(char *input);
 
 #endif /* MINISHELL_H */
