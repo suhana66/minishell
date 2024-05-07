c7cc87a1222d741d86f0d6d0524e7f56a784ae34
Saunthos K Muneer <132748528+saunthosk@users.noreply.github.com>
2024-05-07 18:56:11 +0000

    unset

--- built_in/unset_built_in.c
+++ built_in/unset_built_in.c
@@ -1,104 +1,149 @@
-
 #include "../minishell.h"
 
-int	invalid_identifier(t_data *data, char *str, char *name)
+int	remove_env_var(t_info *info, t_env *to_rem, t_env *p)
 {
-	int	error;
-
-	error = 0;
-	if (!(ft_isalpha(str[0])) && str[0] != '_'
-		&& str[0] != '\"' && str[0] != '\'')
+	if (to_rem)
 	{
-		not_valid_message(str, name, data);
-		error = 1;
-	}
-	return (error);
-}
-
-int	invalid_unset_loop(char *token, char *name, t_data *data)
-{
-	int	i;
-	int	error;
-
-	error = 0;
-	i = 0;
-	if (!ft_strcmp(name, "unset"))
-	{
-		while (token[i])
-		{
-			if (!(ft_isalpha(token[i])) && !ft_isdigit(token[i])
-				&& token[i] != '_' && token[i] != '\"' && token[i] != '\'')
-			{
-				error = 1;
-				break ;
-			}
-			i++;
-		}
-	}
-	if (error == 1)
-	{
-		not_valid_message(token, name, data);
-		return (1);
-	}
-	return (0);
-}
-
-int	remove_env_variable(t_data *data, t_env *to_remove, t_env *prev)
-{
-	if (to_remove)
-	{
-		if (prev)
-			prev->next = to_remove->next;
+		if (p)
+			p->next = to_rem->next;
 		else
-			data->envp = to_remove->next;
-		if (data->envp && !data->envp->key)
+			info->envv = to_rem->next;
+		if (info->envv && !info->envv->key)
 			return (1);
-		free(to_remove->key);
-		free(to_remove->value);
-		free(to_remove);
+		free(to_rem->key);
+		free(to_rem->value);
+		free(to_rem);
 		return (1);
 	}
 	return (0);
 }
 
-int	ft_unset(char *str, t_data *data)
-{
-	char	**token;
-	t_env	*current;
 
-	token = ft_split(str, ' ');
-	current = data->envp;
-	if (ft_strlen(token[0]) != 5)
-		return (free_array(token), ft_error(2, str, data->no_path), 1);
-	if (!unset_loop(data, current, token))
-		return (free_array(token), 0);
-	return (free_array(token), 1);
+int	check_unset_arg(char *str, t_info *info)
+{
+	if (ft_strncmp("PATH", str, 4) == 0)
+	{
+		if (!info->envv)
+			return (0);
+		printf("check if path must be removed or not");
+		return (0);
+	}
+	return (1);
 }
 
-int	unset_loop(t_data *data, t_env *current, char **token)
+int del_var(t_env *env, char **str, t_info *info)
 {
-	int		i;
-	t_env	*remove;
-	t_env	*prev;
+	t_env	*t;
+	t_env	*p;
+	t_env	*k;
+	int	i;
 
 	i = 1;
-	while (token[i])
+	while (str[i])
 	{
-		if (!validate_input(data, token[i], current, "unset"))
-			i++;
-		if (token[i] == NULL)
-			return (1);
-		remove = search_env_variable(data->envp, token[i]);
-		current = data->envp;
-		prev = NULL;
-		check_unset_arg(token[i], data);
-		while (current && current != remove)
+		t = env;
+		while (t)
 		{
-			prev = current;
-			current = current->next;
+			if (ft_strncmp(t->key, str[i], ft_strlen(str[i])) == 0)
+				{
+					k = t;
+			 		break;
+				}
+			t = t->next;
+	}
+	t = env;
+	p = NULL;
+	check_unset_arg(str[i], info);
+	while (t && t != k)
+	{
+		p = t;
+		t = t->next;
+	}
+    remove_env_var(info, k, p);
+	i++;
+}
+return (0);
+}
+
+int	error_unset(t_info *info, t_simple_cmd *simple_cmd)
+{
+	int		i;
+	int j;
+
+	i = 1;
+	if (simple_cmd->str[1] == NULL)
+		{printf("error, not valid identifier");
+			return (1);}
+	//print error as well not enough argument
+	while (simple_cmd->str[i])
+	{
+		if (!(ft_isalpha(simple_cmd->str[i][0])) && simple_cmd->str[i][0] != '_'
+		&& simple_cmd->str[i][0] != '\"' && simple_cmd->str[i][0] != '\'')
+		{
+			printf("error, not valid identifier");
+			return (1);
 		}
-		remove_env_variable(data, remove, prev);
+		if (equal_s(simple_cmd->str[i]) != 0)
+		{
+			printf("error, not valid identifier");
+			return (1);
+		}
+		j = 1;
+		while (simple_cmd->str[i][j])
+		{
+			if (!ft_isalnum(simple_cmd->str[i][j]) && simple_cmd->str[i][j] != '_' && simple_cmd->str[i][j] != '\''  && simple_cmd->str[i][j] != '\"' )
+			 {
+				printf("error, not valid identifier");
+				return (1);
+			}
+			j++;
+		}
 		i++;
 	}
-	return (free(token[i]), 1);
+
+	return (0);
 }
+
+int	ft_unset(t_info *info, t_simple_cmd *simple_cmd)
+{
+	if (error_unset(info, simple_cmd))
+		return (1);
+	else
+		del_var(info->envv, simple_cmd->str, info);
+	return (0);
+}
+
+// int main (int av, char *ac, char **env)
+// {
+//     (void) av;
+//     (void) ac;
+//     t_info  info;
+// 	t_simple_cmd smpl;
+// 	smpl.str = malloc(sizeof(char *) * 4);
+//     if (smpl.str == NULL) {
+//         return 1;
+//     }
+// 	smpl.str[0] = ft_strdup("unset");
+// 	smpl.str[1] = ft_strdup ("SHELL");
+// 	smpl.str[2] = ft_strdup ("HOSTNAME");
+// 	smpl.str[3] = NULL;
+
+// 	info.pwd=NULL;
+// 	info.old_pwd=NULL;
+// 	info.envv=NULL;
+//     //ft_putstr_fd("hello", 1);
+//     env_st(&info,env);
+// 	// int a=error_unset(&info, &smpl);
+// 	// printf("%d", a);
+// 	del_var(info.envv, smpl.str, &info);
+// 	// f_pwd(&info);
+// 	built_env(&info, &smpl);
+//     // //mini_pwd(&info);
+    
+// 	// built_pwd(&info, &smpl);
+	
+// 	// //ft_putstr_fd("hello", 1);
+// 	// cd_built(&info, &smpl);
+// 	// ft_putendl_fd(" ", 1);
+// 	//built_pwd(&info, &smpl);
+// }
