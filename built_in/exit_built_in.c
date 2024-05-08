1c0584846e1987412bcc6ab97cf61d9113681a51
Saunthos K Muneer <132748528+saunthosk@users.noreply.github.com>
2024-05-08 08:10:07 +0000

    exit

--- built_in/exit_built_in.c
+++ built_in/exit_built_in.c
@@ -1,16 +1,46 @@
 #include "../minishell.h"
 
+int	is_str_digit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	determine_exit_code(char **str)
+{
+	int	exit_code;
+
+	if (!str[1])
+		exit_code = 0;
+	else if (is_str_digit(str[1]))
+		exit_code = ft_atoi(str[1]);
+	else
+	{
+		printf("numeric argument required\n");
+		exit_code = 255;
+	}
+	//free_arr(str);
+	exit(exit_code);
+}
+
 int  build_exit(t_info *info, t_simple_cmd *simple_cmd)
 {
-
-    ft_putendl_fd("exit", 2);
+    printf("exit");
     if (simple_cmd->str[1] && simple_cmd->str[2])
 	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		printf("minishell: exit: too many arguments\n");
 		return (1);
 	}
-	str = ft_arrdup(simple_cmd->str);
-	free_tools(tools);
-	determine_exit_code(str);
+	//str = ft_arrdup(simple_cmd->str);
+	// free_tools(tools);
+	determine_exit_code(simple_cmd->str);
 	return (EXIT_SUCCESS);
 }
