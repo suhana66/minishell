8aa0029dcb441236c7746595cf3c27d5832607ad
Suhana Sajid <73544349+Suhana66@users.noreply.github.com>
2024-05-09 12:23:48 +0400

    refactored mini_echo function

--- builtin/mini_echo.c
+++ builtin/mini_echo.c
@@ -6,46 +6,36 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:29:22 by smuneer           #+#    #+#             */
-/*   Updated: 2024/05/08 17:10:55 by susajid          ###   ########.fr       */
+/*   Updated: 2024/05/10 07:23:52 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	n_arg(char **args)
-{
-	int	i;
-
-	i = 0;
-	while (args[i])
-		i++;
-	return (i);
-}
-
 int	mini_echo(t_info *info, t_cmd *simple_cmd)
 {
 	int	i;
 	int	opt;
+	int	j;
 
 	i = 1;
 	opt = 0;
 	(void)info;
-	if (n_arg(simple_cmd->argv) > 1)
+	while (simple_cmd->argv[i] && ft_strncmp(simple_cmd->argv[i], "-n", 2) == 0)
 	{
-		while (simple_cmd->argv[i] && ft_strncmp(simple_cmd->argv[i], "-n", 2) == 0)
-		{
-			opt = 1;
-			i++;
-		}
-		while (simple_cmd->argv[i])
-		{
-			ft_putstr_fd(simple_cmd->argv[i], 1);
-			if (simple_cmd->argv[i + 1] && simple_cmd->argv[i][0] != '\0')
-				write(1, " ", 1);
-			i++;
-		}
+		j = 2;
+		while (simple_cmd->argv[i][j] == 'n')
+			j++;
+		if (simple_cmd->argv[i][j])
+			break ;
+		opt = 1;
+		i++;
 	}
-	if (opt == 0)
-		write(1, "\n", 1);
+	while (simple_cmd->argv[i])
+		if (ft_putstr_fd(simple_cmd->argv[i++], STDOUT_FILENO),
+			simple_cmd->argv[i])
+			ft_putchar_fd(' ', STDOUT_FILENO);
+	if (!opt)
+		ft_putchar_fd('\n', STDOUT_FILENO);
 	return (1);
 }
