d95397bd211acb2623ae0ff7a9912d586ed9c5f6
Saunthos K Muneer <132748528+saunthosk@users.noreply.github.com>
2024-05-19 08:10:36 +0000

    test cd

--- builtin/mini_cd.c
+++ builtin/mini_cd.c
@@ -12,15 +12,43 @@
 
 #include "minishell.h"
 
-void	change_pwd(t_info *info)
+int	get_index_var(char *var, char **env)
+{
+	int		i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(var);
+	while (env[i] && (ft_strncmp(env[i], var, len) || env[i][len] != '='))
+		i++;
+	if (envp[i])
+		return (i);
+	return (-1);
+}
+
+
+
+int	change_pwd(t_info *info, char *old_pd)
 {
 	char	*t;
+	int		flag;
 
-	t = ft_strdup(info->pwd);
+	flag = 0;
+	if (get_index_var("PWD", info->env)
+		&& ft_strlen(env_search(info->env, "OLDPWD")) != 0
+		&& flag == 0)
+	{
+		old_pd = "";
+		flag = 1;
+	}
 	free(info->old_pwd);
-	info->old_pwd = t;
+	info->old_pwd = old_pd;
+	free(old_pd);
+	if (get_index_var("PWD", info->env) != -1)
+		return (1);
 	free(info->pwd);
 	info->pwd = getcwd(NULL, 0);
+	return (0);
 }
 
 int	find_path(t_info *info, char *str)
@@ -81,8 +109,10 @@ void	add_pwd_to_env(t_info *info)
 int	mini_cd(t_info *info, t_cmd *simple_cmd)
 {
 	int	val;
+	char *old_pd;
 
-	if (!simple_cmd->argv[1])
+	old_pd = getcwd(NULL, 0)
+	if (!simple_cmd->argv[1] || (ft_strlen(simple_cmd->argv[1] == 1 && simple_cmd->argv[1][0] == '~')))
 		val = find_path(info, "HOME=");
 	else if (!ft_strncmp(simple_cmd->argv[1], "-", 1) && ft_strlen(simple_cmd->argv[1]) == 1)
 		val = find_path(info, "OLDPWD=");
@@ -98,7 +128,8 @@ int	mini_cd(t_info *info, t_cmd *simple_cmd)
 	}
 	if (val != 0)
 		return (1);
-	change_pwd(info);
+	if(change_pwd(info, old_pd));
+		return (1);
 	add_pwd_to_env(info);
 	return (0);
 }
