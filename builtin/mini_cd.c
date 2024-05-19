0c83f21fd037924c31f79fb96e1fa9f1029f81cf
Saunthos K Muneer <132748528+saunthosk@users.noreply.github.com>
2024-05-19 09:58:25 +0000

    old_pwd

--- builtin/mini_cd.c
+++ builtin/mini_cd.c
@@ -12,43 +12,15 @@
 
 #include "minishell.h"
 
-int	get_index_var(char *var, char **env)
-{
-	int		i;
-	size_t	len;
-
-	i = 0;
-	len = ft_strlen(var);
-	while (env[i] && (ft_strncmp(env[i], var, len) || env[i][len] != '='))
-		i++;
-	if (envp[i])
-		return (i);
-	return (-1);
-}
-
-
-
-int	change_pwd(t_info *info, char *old_pd)
+void	change_pwd(t_info *info)
 {
 	char	*t;
-	int		flag;
 
-	flag = 0;
-	if (get_index_var("PWD", info->env)
-		&& ft_strlen(env_search(info->env, "OLDPWD")) != 0
-		&& flag == 0)
-	{
-		old_pd = "";
-		flag = 1;
-	}
+	t = ft_strdup(info->pwd);
 	free(info->old_pwd);
-	info->old_pwd = old_pd;
-	free(old_pd);
-	if (get_index_var("PWD", info->env) != -1)
-		return (1);
+	info->old_pwd = t;
 	free(info->pwd);
 	info->pwd = getcwd(NULL, 0);
-	return (0);
 }
 
 int	find_path(t_info *info, char *str)
@@ -109,32 +81,24 @@ void	add_pwd_to_env(t_info *info)
 int	mini_cd(t_info *info, t_cmd *simple_cmd)
 {
 	int	val;
-	char *old_pd;
 
-	old_pd = getcwd(NULL, 0)
-	if (!simple_cmd->argv[1] || (ft_strlen(simple_cmd->argv[1] == 1 && simple_cmd->argv[1][0] == '~')))
+	if (!simple_cmd->argv[1])
 		val = find_path(info, "HOME=");
 	else if (!ft_strncmp(simple_cmd->argv[1], "-", 1) && ft_strlen(simple_cmd->argv[1]) == 1)
-	{
 		val = find_path(info, "OLDPWD=");
-		if_print = true;
-	}
 	else
 	{
 		val = chdir(simple_cmd->argv[1]);
 		if (val != 0)
 		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(simple_cmd->argv[1], STDERR_FILENO);
-			ft_putendl_fd(": No such file or directory", STDERR_FILENO);
+			ft_putstr_fd("minishell: ", 2);
+			ft_putstr_fd(simple_cmd->argv[1], 2);
+			// perror(" ");
 		}
 	}
 	if (val != 0)
 		return (1);
-	if(change_pwd(info, old_pd));
-		return (1);
+	change_pwd(info);
 	add_pwd_to_env(info);
-	if (if_print)
-		ft_putendl_fd(info->pwd, STDOUT_FILENO);
 	return (0);
 }
