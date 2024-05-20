af8128a7972a9bafc16c41ebae0e16c49baa418e
Suhana Sajid <73544349+Suhana66@users.noreply.github.com>
2024-05-20 13:30:59 +0400

    fixed expander issue seen during testing

--- expander.c
+++ expander.c
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 14:27:08 by susajid           #+#    #+#             */
-/*   Updated: 2024/05/19 12:13:03 by susajid          ###   ########.fr       */
+/*   Updated: 2024/05/20 13:28:55 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,10 +56,6 @@ int	expand_arg(char **str, char **env)
 		{
 			if ((*str)[i + 1] == '?' && replace_exit_status(str, &i))
 				return (2);
-			else if ((*str)[i + 1] == '!' || (*str)[i + 1] == '$'
-				|| (*str)[i + 1] == '@' || (*str)[i + 1] == '*'
-				|| ft_isdigit((*str)[i + 1]))
-				ft_strlcpy(*str + i, *str + i + 2, ft_strlen(*str + i + 2) + 1);
 			else if (replace_enviornment_variable(str, &i, env))
 				return (1);
 		}
@@ -81,7 +77,7 @@ int	replace_enviornment_variable(char **str, size_t *var_i, char **env)
 	len = 0;
 	while (start[len] && (ft_isalnum(start[len]) || start[len] == '_'))
 		len++;
-	if (len == 0)
+	if (ft_isdigit(start[0]) || len == 0)
 		return ((*var_i)++, 0);
 	temp = ft_substr(start, 0, len);
 	if (!temp)
@@ -137,7 +133,7 @@ char	*env_search(char **env, char *key)
 		if (!ft_strncmp(env[i], key, ft_strlen(key)) && *(env[i] + ft_strlen(key)) == '=')
 		{
 			result = env[i] + ft_strlen(key) + 1;
-			if (*result)
+			if (!*result)
 				return (NULL);
 			else
 				return (result);
